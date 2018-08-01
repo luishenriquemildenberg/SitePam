9603c580ba0fbfa95f480c16dac7e216478553fe
camila monteiro <monteiro.camis@gmail.com>
2018-08-01 20:01:32 -0300

    alt header e projetos

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,42 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
-   
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+$name = $_POST['name'];
+//pega os dados que foi digitado no ID name.
+
+$email = $_POST['email'];
+//pega os dados que foi digitado no ID email.
+
+$subject = $_POST['subject'];
+//pega os dados que foi digitado no ID subject.
+
+$message = $_POST['message'];
+//pega os dados que foi digitado no ID message.
+
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+
+/*abaixo contém os dados que serão enviados para o email
+cadastrado para receber o formulário*/
+
+$corpo = "Formulario enviado pelo site camon.eco.br\n";
+$corpo .= "Nome: " . $name . "\n";
+$corpo .= "Email: " . $email . "\n";
+$corpo .= "Mensagem: " . $message . "\n";
+
+$email_to = 'contato@camon.eco.br';
+//não esqueça de substituir este email pelo seu.
+
+$status = mail($email_to, $subject, $corpo, $headers);
+//enviando o email.
+
+if ($status) {
+  echo "<script> alert('Formulário enviado com sucesso!'); </script>";
+  
+//mensagem de form enviado com sucesso.
+
+} else {
+  echo "<script> alert('Falha ao enviar o Formulário.'); </script>";
+  
+//mensagem de erro no envio. 
+
+}
 ?>
