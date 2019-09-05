96a18762ee10a5037479722fd4aeaba8cf383868
Allan Camargo <allan.cdev@gmail.com>
2019-09-05 12:37:33 -0300

    Alterando para testar

--- mail/environment.php
+++ mail/environment.php
@@ -1,3 +1,10 @@
 <?php 
-    define("ENVIRONMENT", "prod"); // Para colocar em produção basta alterar a const para prod
-    // define("ENVIRONMENT", "development");
+
+    if($_SERVER['HTTP_HOST'] == 'localhost') {
+        define("ENVIRONMENT", "development"); // Subir o projeto como local
+    } else {
+        define("ENVIRONMENT", "prod"); // Para colocar em produção basta alterar a const para prod
+    }
+    
+
+    echo ENVIRONMENT;
