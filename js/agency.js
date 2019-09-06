bb233c457f66afb022ecc0de6419131446261db4
Allan Camargo <allan.cdev@gmail.com>
2019-09-06 00:43:40 -0300

    Alteração logo

--- js/agency.js
+++ js/agency.js
@@ -15,6 +15,7 @@
     }
   });
 
+
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
@@ -30,7 +31,18 @@
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
+      $('#logo').hide();
+      $('#logo-mob').show();
     } else {
+      if($(window).width() < 993) {
+        $('#logo-mob').show();
+        $('#logo').hide();
+      } else {
+        $('#logo').show();
+        $('#logo-mob').hide();
+
+      }
+
       $("#mainNav").removeClass("navbar-shrink");
     }
   };
